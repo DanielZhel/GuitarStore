3fb2d27f3b5920c2b4a6b69b4336f79c54f9bec2
Danielzhel <fartunazh@gmail.com>
2023-01-17 19:03:19 +0300

    Добавьте файлы проекта.

---
+++ StoreDb/StoreDbContext/Entities/Item/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreDb.StoreDbContext.Entities.Item
+{
+    public class Item
+    {
+        public enum ItemType {Guitar, Capo, Pick, Strings, Tuner, Amplifier, Pedal, Cable}
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public string Manufacturer { get; set; }
+        public string ModelName { get; set; }
+        public string Descripton { get; set; }
+        public ItemType Type { get; set; }
+
+        
+
+    }
+}
---
+++ StoreDb/StoreDbContext/StoreDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreDb.StoreDbContext.Entities.Item;
+using StoreDb.StoreDbContext.Entities.User;
+using System.Numerics;
+
+namespace StoreDb.StoreDbContext
+{
+    public class GuitarStoreDbContext : DbContext
+    {
+        public GuitarStoreDbContext(DbContextOptions opts) : base(opts)
+        {
+            Database.EnsureCreated();
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=GuitarStoreDb;Integrated Security = True; MultipleActiveResultSets =True;TrustServerCertificate=True");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Item>().HasKey(k => k.Id);
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.ToTable("Items");
+                entity.Property(p => p.Id).ValueGeneratedNever();
+                entity.Property(p => p.Price).IsRequired();
+                entity.Property(p => p.Type).IsRequired();
+                entity.Property(p => p.Manufacturer).IsRequired();
+            });
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("Users");
+                entity.Property(p => p.Id).ValueGeneratedNever();
+                entity.Property(p => p.Name).IsRequired();
+                entity.Property(p => p.DateOfBirth).IsRequired();
+                entity.Property(p => p.Login).IsRequired();
+                entity.Property(p => p.Password).IsRequired().HasMaxLength(20);
+            });
+
+        }
+
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        
+    }
+}
